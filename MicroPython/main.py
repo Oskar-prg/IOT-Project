14ae06209ef5fb6c0a84a2854739aab1a0931bec
Gennaro <galiemino@gmail.com>
2022-05-14 18:00:37 +0200

    Mqtt implementation and menu interaction

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,7 @@ from time import sleep
 from LCD_i2c_driver import I2cLcd
 from simpleKeyboard import Device
 from binascii import hexlify
+import json
 
 I2C_ADDR = 0x27
 totalRows = 2
@@ -17,6 +18,12 @@ d = Device()
 #Keypad setup
 KEY_UP   = const(0)
 KEY_DOWN = const(1)
+#Albe const
+HIGH = const(1)
+LOW = const(0)
+
+buttonDown = 'B'
+buttonUp = 'A'
 
 keys = [['1', '2', '3', 'A'], ['4', '5', '6', 'B'], ['7', '8', '9', 'C'], ['*', '0', '#', 'D']]
 
@@ -33,12 +40,13 @@ col_pins = [Pin(pin_name, mode=Pin.IN, pull=Pin.PULL_DOWN) for pin_name in cols]
 #Buzzer setup
 buzzer = PWM(Pin(19), freq=440, duty=512)
 
-
+last_message = b''
 # Complete project details at https://RandomNerdTutorials.com
 
 def sub_cb(topic, msg):
     global last_message
     print((topic, msg))
+    
     last_message = msg
     if topic == b'notification' and msg == b'received':
         print('ESP received hello message')
@@ -57,61 +65,106 @@ def restart_and_reconnect():
   time.sleep(10)
   machine.reset()
 
-try:
-  client = connect_and_subscribe()
-except OSError as e:
-  restart_and_reconnect()
 
-while True:
-  try:
-    new_msg = client.check_msg()
-    
-    
-    print(last_message)
-    
-    sleep(1)
+
+
         
 #     if (time.time() - last_message) > message_interval:
 #       msg = b'Hello #%d' % counter
 #       client.publish(topic_pub, msg)
 #       last_message = time.time()
 #       counter += 1
-  except OSError as e:
-    restart_and_reconnect()
+  
+    
+    
+#     for row in range(4):
+#                 for col in range(4):
+#                     key = scan(row, col)
+#                     if key == KEY_DOWN:
+#                         print("Key Pressed", keys[row][col])
+#                         last_key_press = keys[row][col]
+#                         lcd.putstr(str(last_key_press))
+#                         time.sleep_ms(500)
+
+# askPin = True
+# if askPin:
+#                 lcd.clear()
+#                 lcd.putstr("Inserisci pin:\n")
+#                 askPin = False
 
 
 def mainLoop():
+    data = load_r_credentialsFile()
     fromConnecting = True
-    askPin = True
     lcd.putstr("Welcome to \nPassChain")
-    init()
     sleep(2)
+    
     while True:
         if d.isConnected():
             if fromConnecting:
                 connectedBle()
-                fromConnecting = False
                 sleep(2)
-                lcd.clear()
+                fromConnecting = False
+                
+        
+            lcd.clear()
+            select = menuList(data);
+            lcd.clear()
             
-            if askPin:
-                lcd.clear()
-                lcd.putstr("Inserisci pin:\n")
-                askPin = False
+            backButton = False
+            lcd.putstr("> Username: ****")
+            lcd.move_to(0,1)
+            lcd.putstr("> Password: ****")
             
-            for row in range(4):
-                for col in range(4):
-                    key = scan(row, col)
-                    if key == KEY_DOWN:
-                        print("Key Pressed", keys[row][col])
-                        last_key_press = keys[row][col]
-                        lcd.putstr(str(last_key_press))
-                        time.sleep_ms(500)
-#             for i in range(11):
-#                 lcd.putstr(str(10 - i))
-#                 d.send_string(str(10 - i))
-#                 sleep(1)
-#                 lcd.clear()
+            while True:
+                if(not d.isConnected()):
+                    break
+                
+                direction = read_keypad()
+                #time.sleep_ms(160)
+                
+                if direction == 'D':
+                    break
+    
+                if direction == buttonDown:
+                    lcd.clear()
+                    lcd.putstr("Writing")
+                    lcd.move_to(0,1)
+                    lcd.putstr("password.")
+                    time.sleep_ms(250)
+                    lcd.putstr(".")
+                    time.sleep_ms(250)
+                    lcd.putstr(".")
+                    d.send_string(data['credentials'][select]['password'])
+                    time.sleep_ms(250)
+                    lcd.putstr(".")
+                    time.sleep_ms(500)
+                    lcd.clear()
+                    lcd.putstr("> Username: ****")
+                    lcd.move_to(0,1)
+                    lcd.putstr("> Password: ****")
+                    
+        
+                if direction == buttonUp:
+                    lcd.clear()
+                    lcd.putstr("Writing")
+                    lcd.move_to(0,1)
+                    lcd.putstr("username.")
+                    time.sleep_ms(250)
+                    lcd.putstr(".")
+                    time.sleep_ms(250)
+                    lcd.putstr(".")
+                    d.send_string(data['credentials'][select]['username'])
+                    time.sleep_ms(250)
+                    lcd.putstr(".")
+                    time.sleep_ms(500)
+                    lcd.clear()
+                    lcd.putstr("> Username: ****")
+                    lcd.move_to(0,1)
+                    lcd.putstr("> Password: ****")
+                    
+                
+            
         else:
             d.advertise()
             connectingBle()
@@ -167,6 +220,145 @@ def scan(row, col):
 
     # return the key state
     return key
+
+
+#Funzioni Json
+def load_r_credentialsFile():
+   with open('credentials.json') as credentials:
+       return json.load(credentials)
+ 
+ 
+def load_w_credentialsFile(data):
+    with open("credentials.json", "w") as credentials:
+        json.dump(data, credentials) 
+ 
+ 
+def write_credentialsFile(data, name, username, password):
+    if name is not None and not name:
+        if username is not None and not username:
+            if password is not None and not password:
+                new_object = {"name": name,
+                              "username": username,
+                              "password": password
+                              }
+                data["credentials"].append(new_object)   
+                load_w_credentialsFile(data)
+                return True
+    return False
+  
+ 
+def update_credentialsFile(data, name, newName, username, password):
+    if name is not None and not name:
+        for i in range(0, len(data['credentials'])):
+            if data['credentials'][i]['name'] == name:
+                
+                if newName is not None or not newName:
+                    data['credentials'][i]['name'] = newName
+                
+                if username is not None or not username:
+                    data['credentials'][i]['username'] = username
+                
+                if password is not None or not password:
+                    data['credentials'][i]['password'] = password
+                    
+                load_w_credentialsFile(data)
+                return True
+    return False
+ 
+ 
+def remove_credentialsFile(data, name):
+    if name is not None and not name:
+        for i in range(0, len(data['credentials'])):
+            if data['credentials'][i]['name'] == name:
+                del data['credentials'][i]
+                
+                load_w_credentialsFile(data)
+                return True
+    return False
+
+#menu
+def menuList(data):
+    pos = 0
+    lcd.putstr("Authentication")
+    lcd.move_to(0,1)
+    lcd.putstr('> ' + data['credentials'][pos]['name'])
+    while(True):
+        
+        
+        direction = read_keypad()
+        #time.sleep_ms(160)
+        
+        if(direction == buttonDown) and (pos + 1 < len(data['credentials'])):
+            lcd.clear()
+            lcd.putstr(data['credentials'][pos]['name'])
+            lcd.move_to(0,1)
+            pos += 1
+            lcd.putstr('> ' + data['credentials'][pos]['name'])
+            
+        
+        if (direction == buttonUp) and (pos - 1 >= 0):
+            lcd.clear()
+            pos -= 1
+            lcd.putstr('> ' +data['credentials'][pos]['name'])
+            lcd.move_to(0,1)
+            lcd.putstr(data['credentials'][pos+1]['name'])
+            
+        if (direction == '*'):
+            mqtt_connection()
+            pos = 0
+            lcd.putstr("Authentication")
+            lcd.move_to(0,1)
+            lcd.putstr('> ' + data['credentials'][pos]['name'])
+        
+        #if tastierino ritorna pos
+        if read_keypad() == 'C':
+            return pos
+
+def read_keypad():
+    last_key_press = None
+    for row in range(4):
+        for col in range(4):
+            key = scan(row, col)
+            if key == KEY_DOWN:
+                print("Key Pressed", keys[row][col])
+                last_key_press = keys[row][col]
+                #lcd.putstr(str(last_key_press))
+    return last_key_press
+
+def mqtt_connection():
+    global last_message
+    lcd.clear()
+    lcd.putstr("Connecting to")
+    lcd.move_to(0,1)
+    lcd.putstr("PassChain App")
+    try:
+      client = connect_and_subscribe()
+      sleep(1)
+    except OSError as e:
+      restart_and_reconnect()
+    
+    lcd.clear()
+    lcd.putstr("Connected to")
+    lcd.move_to(0,1)
+    lcd.putstr("PassChain App!")
+    
+    while True:
+        direction = read_keypad()
+        try:
+            client.check_msg()
+            if last_message != b'':
+                print(last_message)
+                last_message = b''
+        except OSError as e:
+            restart_and_reconnect()
+            
+        if direction == 'D':
+            client.disconnect()
+            return
+            
+        
+    
+    
 
 if __name__ == "__main__":
     mainLoop()
